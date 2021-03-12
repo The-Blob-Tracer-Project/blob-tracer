13bddb65a954a88fa8df5132a855971423136646
RupakMukherjee <rupakmukherjee01@gmail.com>
2021-03-12 18:57:58 -0500

    first-commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import adios as ad
+import postgkyl as pg
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('seaborn-white')
+import matplotlib.animation as animation
+from random import seed
+from random import random
+from time import sleep
+from tqdm import tqdm
+import os
+import ini
+
+"""
+Project Description:
+
+
+
+
+""""
+
+
+def main():
+    params = ini.parse(open('input.ini').read())
+
+    # Input parameters
+    
